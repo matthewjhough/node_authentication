8547e470af8f6a214e26576c48ad131a427e1c66
matthewjhough <mhoughrsi@rsilogistics.com>
2018-02-21 22:25:21 -0500

    simple server setup

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // app start point
+const express = require('express');
+const http = require('http');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const app = express();
+
+// App setup.
+app.use(morgan('combined'));
+app.use(bodyParser.json({ type: '*/*' }));
+
+// 
+
+
+// server setup
+const port = process.env.PORT || 3090;
+const server = http.createServer(app);
+server.listen(port);
+console.log('server listening on: ', port);
